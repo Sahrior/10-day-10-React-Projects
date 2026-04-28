9549bedee23317cb7251d1220ed0fe7898ce9677
Md Sahrior Haider <165468630+Sahrior@users.noreply.github.com>
2026-04-28 19:15:46 +0600

    Fix Project1 - now normal folder

---
+++ Project1/Brand_Page/src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+        <nav className='container' >
+
+            <div className="logo">
+            <img src="/images/brand_logo.png" alt="Logo" />
+            </div>
+
+            <ul>
+            <li href="#" >Menu</li>
+            <li href="#" >Location</li>
+            <li href="#" >About</li>
+            <li href="#" >Contact</li>
+            </ul>
+
+            <button>login</button>
+
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
