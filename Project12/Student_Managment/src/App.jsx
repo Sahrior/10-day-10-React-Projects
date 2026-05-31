68e3a95e6fc019e0c03ab1021490803752d6d7ff
Md Sahrior Haider <165468630+Sahrior@users.noreply.github.com>
2026-05-31 20:04:21 +0600

    sorting functionality

--- Project12/Student_Managment/src/App.jsx
+++ Project12/Student_Managment/src/App.jsx
@@ -10,6 +10,15 @@ const App = () => {
 
   const [student, setstudent] = useState([])
 
+  const [gradeSort, setGradeSort] = useState([])
+  const [isGradeSort, setIsGradeSort] = useState(false)
+
+  const [alphabetSort, setAlphabetSort] = useState([])
+  const [isAlphabetSort, setIsAlphabetSort] = useState(false)
+
+  const [idSort, setIdSort] = useState([])
+  const [isIdSort, setIsIdSort] = useState(false)
+
   const addStudent = (e) => {
 
     e.preventDefault();
@@ -22,6 +31,62 @@ const App = () => {
     }
 
     setstudent([...student, newStudent])
+
+  }
+
+  const sortById = () =>{
+
+    setIdSort(
+      [...student].sort(
+        (a,b) => a.id - b.id
+      )
+    )
+
+    setIsIdSort(true)
+    setIsAlphabetSort(false);
+    setIsGradeSort(false);
+    
+
+  }
+
+  const sortByAlphabet = () => {
+
+    setAlphabetSort(
+      [...student].sort(
+        (a, b) => a.name.localeCompare(b.name)
+      )
+    )
+
+    setIsAlphabetSort(true);
+    setIsGradeSort(false);
+    setIsIdSort(false)
+
+  }
+
+  const sortStudentByGrade = () => {
+
+    const gradeOrder = {
+      "A+": 1,
+      "A": 2,
+      "A-": 3,
+      "B+": 4,
+      "B": 5,
+      "B-": 6,
+      "C": 7,
+      "D": 8,
+      "F": 9,
+    };
+
+    setGradeSort(
+      [...student].sort(
+        (a,b) => gradeOrder[a.grade] - gradeOrder[b.grade]
+      )
+    );
+
+    setIsGradeSort(true);
+    setIsAlphabetSort(false);
+    setIsIdSort(false)
+
 
   }
 
@@ -39,6 +104,7 @@ const App = () => {
           <input type="text" placeholder='Enter student name'  onChange={(e)=>setName(e.target.value)} />
           <input type="number" placeholder='Enter student id'  onChange={(e)=>setId(e.target.value)} />
           <select onChange={(e) => setGrade(e.target.value)}>
+            <option value="">Select Grade</option>
             <option value="A+">A+</option>
             <option value="A">A</option>
             <option value="A-">A-</option>
@@ -63,21 +129,59 @@ const App = () => {
 
       </div>
 
-      <select className='sort-select' name="" id="">
-        <option value=""> Sort by Grade </option>
-        <option value="">Sort by alphabetical order</option>
+      <select onChange={(e) => { if (e.target.value === "grade") { sortStudentByGrade(); } else if(e.target.value==="alphabet"){ sortByAlphabet(); }  else if(e.target.value==="id") {sortById();}  }} className='sort-select' name="" id="">
+        <option value="none">Select Sort</option>
+        <option value="grade"> Sort by Grade </option>
+        <option value="alphabet">Sort by alphabetical order</option>
+        <option value="id">Sort by ID</option>
       </select>
 
       <div className={student.length==0?"empty-list" : "student-list"}>
-          {student.length==0? showStudent :
-              student.map((student,index)=>(
-                <div className='list' key={index}>
-                  <p> {index+1} </p>
-                  <p>{student.name}</p>
-                  <p>{student.id}</p>
-                  <p>{student.grade}</p>
+          {
+            isGradeSort? (
+              student.length==0? showStudent :
+              gradeSort.map((gradeSort,index)=>(
+                <div className="list" key={index} >
+                    <p>{index+1}</p>
+                    <p>{gradeSort.name}</p>
+                    <p>{gradeSort.id}</p>
+                    <p>{gradeSort.grade}</p>
                 </div>
               ))
+            ) : isAlphabetSort ? (
+              student.length == 0? showStudent :
+              alphabetSort.map((alphabetSort,index)=>(
+                <div className="list" key={index} >
+                  <p>{index+1}</p>
+                  <p> {alphabetSort.name} </p>
+                  <p> {alphabetSort.id} </p>
+                  <p> {alphabetSort.grade} </p>
+                </div> 
+              ))
+            ) : isIdSort ? (
+
+              student.length==0? showStudent : 
+              idSort.map((idSort,index)=>(
+                <div className="list" key={index}>
+                  <p> {index+1} </p>
+                  <p> {idSort.name} </p>
+                  <p> {idSort.id} </p>
+                  <p> {idSort.grade} </p>
+                </div>
+              ))
+
+            ): (
+                student.length==0? showStudent :
+                  student.map((student,index)=>(
+                    <div className='list' key={index}>
+                      <p> {index+1} </p>
+                      <p>{student.name}</p>
+                      <p>{student.id}</p>
+                      <p>{student.grade}</p>
+                    </div>
+                  ))
+              
+            )
           }
       </div>
       
